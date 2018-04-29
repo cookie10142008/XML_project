3e7a634d84c68c33912b900dfc26ef2f932394f6
cookie <cookie10142008@gmail.com>
2018-04-29 22:33:43 +0800

    present xml info on customer table

--- src/ece155b/xml/XMLParser.java
+++ src/ece155b/xml/XMLParser.java
@@ -114,12 +114,49 @@ public class XMLParser extends DefaultHandler {
 		    
 			if(supplyType.equals("sell")) {  // judge item needed or sold
 				sellSupply.ID = value;
-				//System.out.println("add id:"+value);
 			}
 			else if(supplyType.equals("need")) {
 				needSupply.ID = value;
-				//System.out.println("add id:"+value);
-				
+			}
+			
+		}
+		else if(tagName.equals ("SupplyName")) {
+		    
+			if(supplyType.equals("sell")) {  // judge item needed or sold
+				sellSupply.name = value;
+			}
+			else if(supplyType.equals("need")) {
+				needSupply.name = value;
+			}
+			
+		}
+		else if(tagName.equals ("SupplyBrand")) {
+		    
+			if(supplyType.equals("sell")) {  // judge item needed or sold
+				sellSupply.brand = value;
+			}
+			else if(supplyType.equals("need")) {
+				needSupply.brand = value;
+			}
+			
+		}
+		else if(tagName.equals ("SupplyPrice")) {
+		    
+			if(supplyType.equals("sell")) {  // judge item needed or sold
+				sellSupply.price = Double.parseDouble(value);
+			}
+			else if(supplyType.equals("need")) {
+				needSupply.price = Double.parseDouble(value);
+			}
+			
+		}
+		else if(tagName.equals ("SupplyAvailable")) {
+		    
+			if(supplyType.equals("sell")) {  // judge item needed or sold
+				sellSupply.amountAvailable = Integer.parseInt(value);
+			}
+			else if(supplyType.equals("need")) {
+				needSupply.amountNeeded = Integer.parseInt(value);
 			}
 			
 		}
@@ -147,48 +184,57 @@ public class XMLParser extends DefaultHandler {
   		
   		//problem: can't print all items
   		System.out.println(Distributor.sellItems.size() + "*items");
-  		 
+  		int itemCount = Distributor.sellItems.size();
+  		int index = 0;
+  		
   		for(SellSupply item : Distributor.sellItems) {
   	        System.out.println("ID:"+item.ID);
-  	        
+  	        DistributorApplet.custTable.addRow(new Vector()); // new a blank row
   	    }
+  		DistributorApplet.custTable.removeRow(0); // remove the original first row 
+  		
+  		int custRow = DistributorApplet.customerTable.getRowCount(); //get Row Count = 5
+        int custCol = DistributorApplet.customerTable.getColumnCount();  //get Column Count = 5
+  		
+        for(int i = 0; custRow > i; i++){   //from first row to last row
+        	for(int j = 0; custCol > j; j++){  //from first column to last column  		
+        		if(index < itemCount) {
+	        		switch(j) {
+		    			case 0:
+		    				DistributorApplet.customerTable.setValueAt(Distributor.sellItems.get(index).ID,i, j);
+		    				break;
+		    				
+		    			case 1:
+		    				DistributorApplet.customerTable.setValueAt(Distributor.sellItems.get(index).name,i, j);
+		    				break;
+		    				
+		    			case 2:
+		    				DistributorApplet.customerTable.setValueAt(Distributor.sellItems.get(index).brand,i, j);
+		    				break;
+		    				
+		    			case 3:
+		    				DistributorApplet.customerTable.setValueAt(Distributor.sellItems.get(index).price,i, j);
+		    				break;
+		    				
+		    			case 4:
+		    				DistributorApplet.customerTable.setValueAt(Distributor.sellItems.get(index).amountAvailable,i, j);
+		    				break;
+		    				
+	    			
+	        		} //end switch
+        		}// end if
+        		
+        	}//end column
+        	index++;
+        }//end row
+        
   		 
 //  		for (int i = 0; i < Distributor.sellItems.size(); i++) {
 //  			System.out.println(Distributor.sellItems.get(i).ID);
 //		    
 //		} 
   		
-  		/*
-  		int custRow = DistributorApplet.customerTable.getRowCount(); //get Row Count = 5
-        int custCol = DistributorApplet.customerTable.getColumnCount();  //get Column Count = 5
-		String id="", name="", brand="";
-       
-  	// !!! present info on gui !!!
-        for(int i = 0; custRow > i; i++){   //from first row to last row
-        	for(int j = 0; custCol > j; j++){  //from first column to last column  		
-        		switch(j) {
-    			case 0:
-    				DistributorApplet.customerTable.setValueAt(id,i, j);
-    				break;
-    				
-//    			case 1:
-//    				name = (String) DistributorApplet.customerTable.getValueAt(i, j);
-//    				break;
-//    				
-//    			case 2:
-//    				brand = (String) DistributorApplet.customerTable.getValueAt(i, j);
-//    				break;
-//    			
-//    			
-    			
-        		}
-        	}
-        			
-        }
   		
-  		
-  		
-  		*/
   		
   	}// end presentInTable function
   	
